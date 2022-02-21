9a4d8b7eb9fb13e8ef5fdbb853322344250b587c
Анисимова Оксана Михайловна <anisimova.om@main.local>
2022-02-21 21:56:49 +0300

    initial commit

---
+++ src/test/java/ru/netology/AuthTest.java
@@ -0,0 +1,62 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.restassured.RestAssured.given;
+
+public class AuthTest {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+    }
+    @Test
+    void shouldValidLoginPassword() {
+        Registration validUser = DataGenerator.generateValidUser();
+        $("[data-test-id=login] input").setValue(validUser.getLogin());
+        $("[data-test-id=password] input").setValue(validUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldSInvalidPassword() {
+        Registration invalidPassword = DataGenerator.generateInvalidPassword("active");
+        $("[data-test-id=login] input").setValue(invalidPassword.getLogin());
+        $("[data-test-id=password] input").setValue(invalidPassword.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    void shouldInvalidLogin() {
+        Registration invalidLogin = DataGenerator.generateInvalidLogin("active");
+        $("[data-test-id=login] input").setValue(invalidLogin.getLogin());
+        $("[data-test-id=password] input").setValue(invalidLogin.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(text("Неверно указан логин или пароль"));
+    }
+    @Test
+    void shouldBlockedUser() {
+        Registration blockedUser = DataGenerator.generateBlockedUser();
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $(withText("Пользователь заблокирован")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+}
---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,72 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private DataGenerator() {
+    }
+    private static Faker faker = new Faker(new Locale("en"));
+
+        private static RequestSpecification requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://localhost")
+                .setPort(9999)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+
+        static void registrationRequest(Registration registration) {
+            // сам запрос
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(registration) // передаём в теле объект, который будет преобразован в JSON
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 OK
+        }
+
+    public static String generateLogin() {
+        String login = faker.name().username();
+        return login;
+    }
+
+    public static String generatePassword() {
+        String password = faker.internet().password(false);
+        return password;
+    }
+
+    public static Registration generateValidUser() {
+        Registration registration = new Registration(generateLogin(), generatePassword(), "active");
+        registrationRequest(registration);
+        return registration;
+    }
+
+    public static Registration generateBlockedUser() {
+        Registration registration = new Registration(generateLogin(), generatePassword(), "blocked");
+        registrationRequest(registration);
+        return registration;
+    }
+
+    public static Registration generateInvalidPassword(String status) {
+        String login = generateLogin();
+        registrationRequest(new Registration(login, generatePassword(), status));
+        return new Registration(login, generatePassword(), status);
+    }
+
+    public static Registration generateInvalidLogin(String status) {
+        String password = generatePassword();
+        registrationRequest(new Registration(generateLogin(), password, status));
+        return new Registration(generateLogin(), password, status);
+    }
+}
